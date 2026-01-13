ac9172ddc6989a3a69a346f2193c28c524b65687
alexisTobar <tobaralexis.89@gmail.com>
2026-01-13 09:37:24 -0300

    axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api' // La URL de tu servidor
+    // VITE_API_URL será la dirección que nos dé Render más adelante
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api' 
 });
 
 export default instance;
