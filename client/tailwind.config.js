bbc591c1e2f241213c0bb8d2edd40b9384f2ae77
alexisTobar <tobaralexis.89@gmail.com>
2026-01-14 00:28:36 -0300

    home

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // <--- ESTA LÍNEA ES LA QUE ACTIVA EL BOTÓN DE SOL/LUNA
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
